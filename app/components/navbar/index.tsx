c66ac1ba5340bf22f8e279e716d5b9d7ababcec2
pinal-pixer <pinal.patoliya@pixer.digital>
2024-07-26 14:42:26 +0530

    create font and color variable file and create navbar design

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -1,7 +1,178 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import {
+  CountText,
+  ItemDiv,
+  ItemMainDiv,
+  Label,
+  LeftDiv,
+  LogoDiv,
+  MainDiv,
+  Title,
+  TopDiv,
+} from "./style";
+import SVGIcon from "@/app/assets/svg/SVGIcon";
+import { colors } from "@/app/styles/colors";
 
 function Navbar() {
-  return <div>Navbar component</div>;
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const handleClick = (index: number) => {
+    setActiveIndex(index === activeIndex ? null : index); // Toggle active state
+  };
+
+  return (
+    <MainDiv>
+      <TopDiv>
+        <LogoDiv>
+          <SVGIcon
+            name="logo-icon"
+            width="101"
+            height="28"
+            viewBox="0 0 101 28"
+            fill="none"
+            className="logo-icon"
+          />
+          <SVGIcon
+            name="sidebar-icon"
+            width="28"
+            height="28"
+            viewBox="0 0 28 28"
+            fill="none"
+            className="logo-icon"
+          />
+        </LogoDiv>
+        <ItemDiv isActive={activeIndex === 0} onClick={() => handleClick(0)}>
+          <LeftDiv>
+            <SVGIcon
+              name="started-icon"
+              width="12"
+              height="12"
+              fill={activeIndex === 0 ? colors.icon_hover : colors.icon}
+              viewBox="0 0 12 12"
+            />
+            <Title isActive={activeIndex === 0}>Getting started</Title>
+          </LeftDiv>
+        </ItemDiv>
+        <ItemMainDiv>
+          <ItemDiv isActive={activeIndex === 1} onClick={() => handleClick(1)}>
+            <LeftDiv>
+              <SVGIcon
+                name="inbox-icon"
+                width="12"
+                height="12"
+                fill={activeIndex === 1 ? colors.icon_hover : colors.icon}
+                viewBox="0 0 12 12"
+              />
+              <Title isActive={activeIndex === 1}>Inbox</Title>
+            </LeftDiv>
+            <CountText>4</CountText>
+          </ItemDiv>
+          <ItemDiv isActive={activeIndex === 2} onClick={() => handleClick(2)}>
+            <LeftDiv>
+              <SVGIcon
+                name="inbox-icon"
+                width="12"
+                height="12"
+                fill={activeIndex === 2 ? colors.icon_hover : colors.icon}
+                viewBox="0 0 12 12"
+              />
+              <Title isActive={activeIndex === 2}>Unassigned</Title>
+            </LeftDiv>
+          </ItemDiv>
+          <ItemDiv isActive={activeIndex === 3} onClick={() => handleClick(3)}>
+            <LeftDiv>
+              <SVGIcon
+                name="inbox-icon"
+                width="12"
+                height="12"
+                fill={activeIndex === 3 ? colors.icon_hover : colors.icon}
+                viewBox="0 0 12 12"
+              />
+              <Title isActive={activeIndex === 3}>All</Title>
+            </LeftDiv>
+          </ItemDiv>
+        </ItemMainDiv>
+        <ItemMainDiv>
+          <ItemDiv isActive={activeIndex === 4} onClick={() => handleClick(4)}>
+            <LeftDiv>
+              <SVGIcon
+                name="inbox-icon"
+                width="12"
+                height="12"
+                fill={activeIndex === 4 ? colors.icon_hover : colors.icon}
+                viewBox="0 0 12 12"
+              />
+              <Title isActive={activeIndex === 4}>Contacts</Title>
+            </LeftDiv>
+          </ItemDiv>
+          <ItemDiv isActive={activeIndex === 5} onClick={() => handleClick(5)}>
+            <LeftDiv>
+              <SVGIcon
+                name="inbox-icon"
+                width="12"
+                height="12"
+                fill={activeIndex === 5 ? colors.icon_hover : colors.icon}
+                viewBox="0 0 12 12"
+              />
+              <Title isActive={activeIndex === 5}>Insights</Title>
+            </LeftDiv>
+          </ItemDiv>
+        </ItemMainDiv>
+        <ItemMainDiv>
+          <Label>Label</Label>
+          <ItemDiv isActive={activeIndex === 6} onClick={() => handleClick(6)}>
+            <LeftDiv>
+              <SVGIcon
+                name="inbox-icon"
+                width="12"
+                height="12"
+                fill={activeIndex === 6 ? colors.icon_hover : colors.icon}
+                viewBox="0 0 12 12"
+              />
+              <Title isActive={activeIndex === 6}>Bug</Title>
+            </LeftDiv>
+          </ItemDiv>
+          <ItemDiv isActive={activeIndex === 7} onClick={() => handleClick(7)}>
+            <LeftDiv>
+              <SVGIcon
+                name="inbox-icon"
+                width="12"
+                height="12"
+                fill={activeIndex === 7 ? colors.icon_hover : colors.icon}
+                viewBox="0 0 12 12"
+              />
+              <Title isActive={activeIndex === 7}>Question</Title>
+            </LeftDiv>
+          </ItemDiv>
+          <ItemDiv isActive={activeIndex === 8} onClick={() => handleClick(8)}>
+            <LeftDiv>
+              <SVGIcon
+                name="inbox-icon"
+                width="12"
+                height="12"
+                fill={activeIndex === 8 ? colors.icon_hover : colors.icon}
+                viewBox="0 0 12 12"
+              />
+              <Title isActive={activeIndex === 8}>Feedback</Title>
+            </LeftDiv>
+          </ItemDiv>
+        </ItemMainDiv>
+      </TopDiv>
+      <ItemDiv isActive={activeIndex === 9} onClick={() => handleClick(9)}>
+        <LeftDiv>
+          <SVGIcon
+            name="started-icon"
+            width="12"
+            height="12"
+            fill={activeIndex === 9 ? colors.icon_hover : colors.icon}
+            viewBox="0 0 12 12"
+          />
+          <Title isActive={activeIndex === 9}>Support</Title>
+        </LeftDiv>
+      </ItemDiv>
+    </MainDiv>
+  );
 }
 
 export default Navbar;
--- app/components/navbar/style.tsx
+++ app/components/navbar/style.tsx
@@ -1,5 +1,95 @@
+import { colors } from "@/app/styles/colors";
+import { Typography } from "@/app/styles/typography";
 import styled from "styled-components";
 
-const StyledDivExample = styled.div``;
+interface Props {
+  isActive?: boolean;
+}
 
-export { StyledDivExample };
+const MainDiv = styled.div`
+  padding: 20px 16px;
+  height: 100vh;
+  background-color: ${colors.bg_surface};
+  width: 223px;
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const TopDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const LogoDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .logo-icon {
+    cursor: pointer;
+    &:hover {
+      background-color: ${colors.bg_surface_hover};
+      border-radius: 30px;
+    }
+  }
+`;
+
+const ItemMainDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1px;
+`;
+
+const ItemDiv = styled.div<Props>`
+  display: flex;
+  align-items: center;
+  padding: 4px 4px 4px 12px;
+  justify-content: space-between;
+  cursor: pointer;
+  background-color: ${({ isActive }) => isActive && colors.bg_surface_active};
+  border-radius: ${({ isActive }) => isActive && "30px"};
+  &:hover {
+    background-color: ${colors.bg_surface_hover};
+    border-radius: 30px;
+  }
+`;
+
+const Title = styled.p<Props>`
+  ${Typography.body_md_regular}
+  color: ${({ isActive }) =>
+    isActive ? colors.text : colors.text_text_secondary};
+`;
+
+const Label = styled.p`
+  ${Typography.body_sm_medium}
+  color: ${colors.text_text_secondary};
+  padding: 0 12px;
+`;
+
+const CountText = styled.p`
+  ${Typography.body_sm_regular}
+  color: ${colors.text_text_secondary};
+  background-color: ${colors.bg_surface_secondary};
+  padding: 2px 8px;
+  border-radius: 50%;
+`;
+
+const LeftDiv = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export {
+  MainDiv,
+  LogoDiv,
+  ItemDiv,
+  Title,
+  ItemMainDiv,
+  Label,
+  TopDiv,
+  CountText,
+  LeftDiv,
+};
