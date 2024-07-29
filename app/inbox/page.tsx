03424008a91d61d8f79fbd256aec2f837b3125ff
pinal-pixer <pinal.patoliya@pixer.digital>
2024-07-29 11:22:33 +0530

    update all color

--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -17,6 +17,28 @@ export default function Inbox() {
   const [activeTab, setActiveTab] = useState("Open");
   const tabItem = ["Open", "Snoozed", "Done"];
 
+  const statuses1 = [
+    { title: "Question", isName: false, iconName: "question-icon" },
+    { title: "Priority", isName: false, iconName: "inbox-icon" },
+    {
+      title: "Sanjay M.",
+      isName: true,
+      avatarUrl:
+        "https://firebasestorage.googleapis.com/v0/b/teamcamp-app.appspot.com/o/UserProfiles%2FUntitled1_1701236653470.jpg?alt=media&token=8bc07cdb-5fcc-4c69-8e0d-c9978b94b3e4",
+    },
+  ];
+
+  const statuses2 = [
+    { title: "Bug", isName: false, iconName: "bug-icon" },
+    { title: "Priority", isName: false, iconName: "inbox-icon" },
+    {
+      title: "Sanjay M.",
+      isName: true,
+      avatarUrl:
+        "https://firebasestorage.googleapis.com/v0/b/teamcamp-app.appspot.com/o/UserProfiles%2FUntitled1_1701236653470.jpg?alt=media&token=8bc07cdb-5fcc-4c69-8e0d-c9978b94b3e4",
+    },
+  ];
+
   return (
     <Main>
       <NavbarPage />
@@ -46,6 +68,7 @@ export default function Inbox() {
             }
             time={"3 min ago"}
             showDotIcon={true}
+            status={statuses1}
           />
           <InboxCard
             name={"Bhavdip Patel from Google"}
@@ -54,6 +77,7 @@ export default function Inbox() {
               "Complete your registration to activate your teamcamp subscription Complete your registration to activate your teamcamp subscription..."
             }
             time={"10 min ago"}
+            status={statuses2}
           />
         </BottomDiv>
       </MainDiv>
