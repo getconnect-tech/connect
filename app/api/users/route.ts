48ec3ff59f6ff1b67e626fc799902514640677f5
Vatsal Ghoghari <vatsal.ghoghari@pixer.digital>
2024-07-31 12:22:32 +0530

    added services and APIs for signup

---
+++ app/api/users/route.ts
@@ -0,0 +1,26 @@
+import { isEmpty } from "@/helpers/common";
+import { createUser, isUserAlreadyExists } from "@/services/userServices";
+import { NextRequest } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const { email, displayName, profilePic } = await req.json();
+
+    if (isEmpty(email)) {
+      return Response.json({ error: "'email' is required in request body!" }, { status: 400 });
+    }
+
+    // Check if user already exists on database
+    const isValidEmail = await isUserAlreadyExists(email);
+    if (isValidEmail) {
+      return Response.json({ error: "Account already exsists!" }, { status: 409 });
+    }
+
+    // if not create a new unverififed user
+    const newUser = await createUser({ email, profilePic, displayName });
+
+    return Response.json(newUser, { status: 201 });
+  } catch (err: any) {
+    return Response.json({ error: err.message }, { status: 500 });
+  }
+};
