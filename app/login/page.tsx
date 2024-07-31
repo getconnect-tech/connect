81108cd89e41f1d08865c538655abfeb2c55d3c6
Vatsal Ghoghari <vatsal.ghoghari@pixer.digital>
2024-07-31 16:06:43 +0530

    made counter dynamic

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,49 +1,54 @@
 "use client";
-import React, { useCallback, useState } from "react";
-import {
-  Bottom,
-  CodeSection,
-  Form,
-  Heading,
-  LoginSection,
-  LoginText,
-  MainDiv,
-  TimeText
-} from "./style";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { Bottom, CodeSection, Form, Heading, LoginSection, LoginText, MainDiv, TimeText } from "./style";
 import SVGIcon from "@/assets/icons/SVGIcon";
 import Input from "@/components/input/input";
 import Button from "@/components/button/button";
 
+let hasInterval = false;
 export default function LoginPage() {
   const [showBottomSection, setShowBottomSection] = useState(false);
+  const [counter, setCounter] = useState(5 * 60);
 
   const handleContinueClick = useCallback(() => {
     setShowBottomSection(true);
   }, []);
+
+  useEffect(() => {
+    if (hasInterval) return;
+    hasInterval = true;
+    const counterInterval = setInterval(() => {
+      setCounter((prev) => {
+        if (prev === 0) {
+          clearInterval(counterInterval);
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+  }, []);
+
+  const Counter = useMemo(() => {
+    const minutes = Math.floor(counter / 60).toString();
+    const seconds = (counter % 60).toString();
+    return `${minutes.length <= 1 ? "0" + minutes : minutes}:${seconds.length <= 1 ? "0" + seconds : seconds}`;
+  }, [counter]);
+
+  const resendCode = () => {};
 
   return (
     <div>
       <MainDiv>
         <LoginSection>
           <Heading>
-            <SVGIcon
-              name="secondary-logo"
-              width="60px"
-              height="60px"
-              viewBox="0 0 60 60"
-            />
+            <SVGIcon name="secondary-logo" width="60px" height="60px" viewBox="0 0 60 60" />
             <LoginText>Welcome to Connect</LoginText>
           </Heading>
           {!showBottomSection ? (
             <>
               <Form>
                 <Input type={"text"} placeholder="Email address" />
-                <Button
-                  title="Continue"
-                  width={true}
-                  className="button"
-                  onClick={handleContinueClick}
-                />
+                <Button title="Continue" width={true} className="button" onClick={handleContinueClick} />
               </Form>
               <Bottom>
                 <p>By continuing, you are indicating that you have</p>
@@ -58,14 +63,13 @@ export default function LoginPage() {
           ) : (
             <CodeSection>
               <p>
-                We have sent a temporary code to{" "}
-                <span>mohit.gajera@pixer.digital.</span>
+                We have sent a temporary code to <span>mohit.gajera@pixer.digital.</span>
               </p>
               <Input placeholder={"Enter Code"} type={"number"} />
               <Button title="Login" isWidth width />
               <TimeText>
-                04:49
-                <span>Resend Code</span>
+                {Counter}
+                <span onClick={resendCode}>Resend Code</span>
               </TimeText>
             </CodeSection>
           )}
