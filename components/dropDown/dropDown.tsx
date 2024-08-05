54f82cec5e7decf1ed9f9e9f60ec4e69b06d4446
pinal-pixer <pinal.patoliya@pixer.digital>
2024-08-05 15:04:11 +0530

    Update dropDown.tsx

--- components/dropDown/dropDown.tsx
+++ components/dropDown/dropDown.tsx
@@ -52,91 +52,81 @@ export const useOutsideClick = (callback: () => void) => {
   return ref;
 };
 
-const DropDown = React.forwardRef<HTMLDivElement, DropDownProps>(
-  (
-    {
-      items,
-      style,
-      iconSize,
-      iconViewBox,
-      onClose,
-      isSearch = false,
-      isCheckbox = false,
-    },
-    ref
-  ) => {
-    const dropDownRef = useOutsideClick(onClose);
-    const [hoveredItem, setHoveredItem] = useState<string | null>(null);
-    const [selectedItems, setSelectedItems] = useState<{
-      [key: string]: boolean;
-    }>({});
+export default function DropDown({
+  items,
+  style,
+  iconSize,
+  iconViewBox,
+  onClose,
+  isSearch = false,
+  isCheckbox = false,
+}: DropDownProps) {
+  const dropDownRef = useOutsideClick(onClose);
+  const [hoveredItem, setHoveredItem] = useState<string | null>(null);
+  const [selectedItems, setSelectedItems] = useState<{
+    [key: string]: boolean;
+  }>({});
 
-    const handleItemClick = useCallback((name: string) => {
-      setSelectedItems((prevState) => ({
-        ...prevState,
-        [name]: !prevState[name],
-      }));
-    }, []);
+  const handleItemClick = useCallback((name: string) => {
+    setSelectedItems((prevState) => ({
+      ...prevState,
+      [name]: !prevState[name],
+    }));
+  }, []);
 
-    const handleMouseEnter = (name: string) => {
-      setHoveredItem(name);
-    };
+  const handleMouseEnter = (name: string) => {
+    setHoveredItem(name);
+  };
 
-    const handleMouseLeave = () => {
-      setHoveredItem(null);
-    };
-
-    return (
-      <MainDiv ref={dropDownRef} style={style}>
-        {isSearch && (
-          <SearchDiv>
-            <Input
-              placeholder={"Search"}
-              style={{ border: "none", padding: "8px 12px" }}
-              autoFocus={true}
-              iconName="search-icon"
-              iconSize="12"
-              iconViewBox="0 0 12 12"
-              isIcon={true}
-            />
-          </SearchDiv>
-        )}
-        <ItemMainDiv>
-          {items.map((item, index) => (
-            <ItemDiv
-              key={index}
-              isSelected={!!selectedItems[item.name]}
-              isHovered={hoveredItem === item.name}
-              onClick={() => handleItemClick(item.name)}
-              onMouseEnter={() => handleMouseEnter(item.name)}
-              onMouseLeave={handleMouseLeave}
-            >
-              {isCheckbox && (
-                <StyledCheckbox
-                  checked={selectedItems[item.name] || false}
-                  onChange={() => handleItemClick(item.name)}
-                />
-              )}
-              {item.icon && (
-                <SVGIcon
-                  name={item.icon}
-                  width={iconSize}
-                  height={iconSize}
-                  viewBox={iconViewBox}
-                />
-              )}
-              {item.src && item.isName && (
-                <Avatar name="" imgSrc={item.src} size={20} />
-              )}
-              <p>{item.name}</p>
-            </ItemDiv>
-          ))}
-        </ItemMainDiv>
-      </MainDiv>
-    );
-  }
-);
-
-DropDown.displayName = "DropDown";
-
-export default DropDown;
+  const handleMouseLeave = () => {
+    setHoveredItem(null);
+  };
+  return (
+    <MainDiv ref={dropDownRef} style={style}>
+      {isSearch && (
+        <SearchDiv>
+          <Input
+            placeholder={"Search"}
+            style={{ border: "none", padding: "8px 12px" }}
+            autoFocus={true}
+            iconName="search-icon"
+            iconSize="12"
+            iconViewBox="0 0 12 12"
+            isIcon={true}
+          />
+        </SearchDiv>
+      )}
+      <ItemMainDiv>
+        {items.map((item, index) => (
+          <ItemDiv
+            key={index}
+            isSelected={!!selectedItems[item.name]}
+            isHovered={hoveredItem === item.name}
+            onClick={() => handleItemClick(item.name)}
+            onMouseEnter={() => handleMouseEnter(item.name)}
+            onMouseLeave={handleMouseLeave}
+          >
+            {isCheckbox && (
+              <StyledCheckbox
+                checked={selectedItems[item.name] || false}
+                onChange={() => handleItemClick(item.name)}
+              />
+            )}
+            {item.icon && (
+              <SVGIcon
+                name={item.icon}
+                width={iconSize}
+                height={iconSize}
+                viewBox={iconViewBox}
+              />
+            )}
+            {item.src && item.isName && (
+              <Avatar name="" imgSrc={item.src} size={20} />
+            )}
+            <p>{item.name}</p>
+          </ItemDiv>
+        ))}
+      </ItemMainDiv>
+    </MainDiv>
+  );
+}
