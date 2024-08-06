a22e30fa361f29410c69f94237eb3b237a34bfc1
laxitarajpurohit <laxita.rajpurohit@pixer.digital>
2024-08-06 12:56:03 +0530

    create assign contextMenu

--- components/contextMenu/contextMenu.tsx
+++ components/contextMenu/contextMenu.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+/* eslint-disable max-len */
+import React, { useState } from 'react';
 import * as ContextMenu from '@radix-ui/react-context-menu';
+import DropDown from '../dropDown/dropDown';
 import {
   ContextMenuContent,
   ContextMenuItem,
   ContextMenuMainDiv,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
 } from './style';
 import SVGIcon from '@/assets/icons/SVGIcon';
 
@@ -12,29 +16,67 @@ interface Props {
 }
 
 export default function CustomContextMenu({ children }: Props) {
+  const [assignDropdown, setAssignDropdown] = useState(false);
+
+  const assignItem = [
+    { name: 'Unassigned', icon: 'dropdown-unassign-icon' },
+    {
+      name: 'Sanjay M.',
+      src: 'https://firebasestorage.googleapis.com/v0/b/teamcamp-app.appspot.com/o/UserProfiles%2FUntitled1_1701236653470.jpg?alt=media&token=8bc07cdb-5fcc-4c69-8e0d-c9978b94b3e4',
+      isName: true,
+    },
+    {
+      name: 'Aniket',
+      src: 'https://bearbuk.blob.core.windows.net/project/Profile_63c0ec5555376218700f12d5_2023041410225842.png',
+      isName: true,
+    },
+    {
+      name: 'Jemish',
+      src: 'https://firebasestorage.googleapis.com/v0/b/teamcamp-app.appspot.com/o/UserProfiles%2FUntitled1_1701236653470.jpg?alt=media&token=8bc07cdb-5fcc-4c69-8e0d-c9978b94b3e4',
+      isName: true,
+    },
+    {
+      name: 'Vatsal',
+      src: 'https://firebasestorage.googleapis.com/v0/b/teamcamp-app.appspot.com/o/UserProfiles%2F1708409574833_1712819712813.jpg?alt=media&token=42df7e19-9083-4c61-8b51-b43d5c3f4183',
+      isName: true,
+    },
+  ];
   return (
     <ContextMenuMainDiv>
       <ContextMenu.Root>
         <ContextMenu.Trigger asChild>{children}</ContextMenu.Trigger>
         <ContextMenu.Portal>
           <ContextMenuContent>
-            <ContextMenuItem>
-              <div>
+            <ContextMenu.Sub>
+              <ContextMenuSubTrigger>
+                <div>
+                  <SVGIcon
+                    name='context-assign-icon'
+                    width='12'
+                    height='12'
+                    viewBox='0 0 12 12'
+                  />
+                  Assigned to
+                </div>
                 <SVGIcon
-                  name='context-assign-icon'
-                  width='12'
-                  height='12'
-                  viewBox='0 0 12 12'
+                  name='context-arrow-icon'
+                  width='10'
+                  height='10'
+                  viewBox='0 0 10 10'
                 />
-                Assigned to
-              </div>
-              <SVGIcon
-                name='context-arrow-icon'
-                width='10'
-                height='10'
-                viewBox='0 0 10 10'
-              />
-            </ContextMenuItem>
+              </ContextMenuSubTrigger>
+              <ContextMenu.Portal>
+                <ContextMenuSubContent className='ContextMenuSubContent'>
+                  <DropDown
+                    items={assignItem}
+                    iconSize='20'
+                    iconViewBox='0 0 20 20'
+                    onClose={() => setAssignDropdown(false)}
+                    isSearch={true}
+                  />
+                </ContextMenuSubContent>
+              </ContextMenu.Portal>
+            </ContextMenu.Sub>
             <ContextMenuItem>
               <div>
                 <SVGIcon
--- components/contextMenu/style.tsx
+++ components/contextMenu/style.tsx
@@ -44,4 +44,62 @@ const ContextMenuItem = styled(ContextMenu.Item)`
   }
 `;
 
-export { ContextMenuMainDiv, ContextMenuContent, ContextMenuItem };
+const ContextMenuSubTrigger = styled(ContextMenu.SubTrigger)`
+  ${Typography.body_md_regular};
+  color: ${colors.text_text_secondary};
+  border: none;
+  outline: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 4px 8px;
+  div {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+  &:hover {
+    background-color: ${colors.bg_white_hover};
+    border-radius: 6px;
+    color: ${colors.text};
+  }
+  /* display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 4px 8px; */
+  position: relative;
+  &:hover {
+    background-color: ${colors.bg_white_hover};
+    border-radius: 6px;
+    color: ${colors.text};
+  }
+`;
+
+const ContextMenuSubContent = styled(ContextMenu.SubContent)`
+  /* background-color: ${colors.bg_white};
+  box-shadow:
+    0px 0px 0px 0.5px ${colors.box_shadow},
+    0px 4px 8px 0px ${colors.box_shadow},
+    0px 8px 24px 0px ${colors.box_shadow}; */
+  border-radius: 12px;
+  padding: 4px;
+  z-index: 2;
+  min-width: 180px;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 100%;
+  margin-left: 4px;
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+`;
+
+export {
+  ContextMenuMainDiv,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuSubTrigger,
+  ContextMenuSubContent,
+};
