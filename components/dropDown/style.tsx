aa34c7d04dc13e0cf64361d5f6c43b7a3e18b408
pinal-pixer <pinal.patoliya@pixer.digital>
2024-08-10 13:05:01 +0530

    Update style.tsx

--- components/dropDown/style.tsx
+++ components/dropDown/style.tsx
@@ -98,9 +98,7 @@ const StyledCheckbox = styled.input.attrs({ type: 'checkbox' })`
 
   &:checked {
     background-color: ${colors.brand};
-    background-image: url('data:image/svg+xml;utf8,${encodeURIComponent(
-      checkmarkSVG,
-    )}');
+    background-image: url('data:image/svg+xml;utf8,${encodeURIComponent(checkmarkSVG)}');
     background-size: 10px 10px;
     background-repeat: no-repeat;
     background-position: center;
