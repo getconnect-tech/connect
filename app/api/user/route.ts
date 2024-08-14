c98b1d41c2604420f2896750d76c8e3f2c2f80ec
Vatsal Ghoghari <vatsal.ghoghari@pixer.digital>
2024-08-14 17:16:01 +0530

    implement service and API to get current user

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,7 +2,21 @@ import { z } from 'zod';
 import { displayNameSchema, profilePicSchema } from '@/lib/zod/user';
 import withAuth from '@/middlewares/withAuth';
 import { handleApiError } from '@/helpers/errorHandler';
-import { updateUser } from '@/services/serverSide/user';
+import { getUserById, updateUser } from '@/services/serverSide/user';
+
+export const GET = withAuth(async (req) => {
+  try {
+    const user = await getUserById(req.user.id);
+
+    if (!user) {
+      return Response.json({ error: 'Not Found!' }, { status: 404 });
+    }
+
+    return Response.json(user, { status: 200 });
+  } catch (err) {
+    return handleApiError(err);
+  }
+});
 
 const RequestBody = z.object({
   dispalyName: displayNameSchema.optional(),
