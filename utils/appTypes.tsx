f7883902128f019e92b4e28c85c0d06e8a7d8af2
Vatsal Ghoghari <vatsal.ghoghari@pixer.digital>
2024-08-14 17:21:28 +0530

    get assigned user details in ticket

--- utils/appTypes.tsx
+++ utils/appTypes.tsx
@@ -1,5 +1,8 @@
-import { Contact, Ticket } from '@prisma/client';
+import { Contact } from '@prisma/client';
+import { formatTicket } from '@/services/serverSide/ticket';
 
 export interface TicketDetailsInterface extends Ticket {
   contact?: Contact;
 }
+
+export type Ticket = NonNullable<Awaited<ReturnType<typeof formatTicket>>>;
