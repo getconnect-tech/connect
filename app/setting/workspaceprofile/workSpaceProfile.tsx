2e68e173e327c8098a809243a729a876ae714db8
jemin-pixer <jemin.rudani@pixer.digital>
2024-08-16 16:25:49 +0530

    every function use useCallback

--- app/setting/workspaceprofile/workSpaceProfile.tsx
+++ app/setting/workspaceprofile/workSpaceProfile.tsx
@@ -54,11 +54,11 @@ const WorkspaceProfile = () => {
 
   const inputRef = useRef<HTMLInputElement | null>(null);
 
-  const handleUploadClick = () => {
+  const handleUploadClick = useCallback(() => {
     inputRef.current?.click();
-  };
+  }, []);
 
-  const convertBase64 = (file: File) => {
+  const convertBase64 = useCallback((file: File) => {
     return new Promise((resolve, reject) => {
       const fileReader = new FileReader();
       fileReader.readAsDataURL(file);
@@ -73,41 +73,43 @@ const WorkspaceProfile = () => {
         reject(error);
       };
     });
-  };
+  }, []);
 
-  const handleFileRead = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    try {
-      const file = event.target.files?.[0];
-      if (!file) {
-        alert('No file selected.');
-        return;
+  const handleFileRead = useCallback(
+    async (event: React.ChangeEvent<HTMLInputElement>) => {
+      try {
+        const file = event.target.files?.[0];
+        if (!file) {
+          alert('No file selected.');
+          return;
+        }
+        const fileData = file.name.split('.');
+        if (file.size > 500000) {
+          alert('Please upload less than 500kb photo size.');
+          return false;
+        }
+        if (
+          fileData[1] === 'jpg' ||
+          fileData[1] === 'jpeg' ||
+          fileData[1] === 'png'
+        ) {
+          await convertBase64(file);
+        } else {
+          alert('Please upload a valid type photo.');
+          return false;
+        }
+      } catch (e) {
+        console.log('error', e);
       }
-      const fileData = file.name.split('.');
-      if (file.size > 500000) {
-        alert('Please upload less than 500kb photo size.');
-        return false;
-      }
-      if (
-        fileData[1] === 'jpg' ||
-        fileData[1] === 'jpeg' ||
-        fileData[1] === 'png'
-      ) {
-        await convertBase64(file);
-      } else {
-        alert('Please upload a valid type photo.');
-        return false;
-      }
-    } catch (e) {
-      console.log('error', e);
-    }
-  };
+    },
+    [],
+  );
 
   const handleUpdate = useCallback(
     async (e: SyntheticEvent) => {
       e.preventDefault();
       try {
         workspaceStore.setLoading(true);
-        const currentOrganizationName = organizationName;
         let profile;
         if (image) {
           profile = await getFirebaseUrlFromFile(image?.file, 'UserProfiles');
@@ -122,7 +124,7 @@ const WorkspaceProfile = () => {
           name: string;
           image_url: string | null;
         } = {
-          name: currentOrganizationName,
+          name: organizationName,
           image_url: updatedImege,
         };
         await updateWorkspaceDetails(payload);
@@ -166,18 +168,15 @@ const WorkspaceProfile = () => {
                 ref={inputRef}
                 style={{ display: 'none' }}
               />
-              {!isEmpty(image?.profile || currentWorkspace?.image_url) && (
-                <Avatar
-                  size={58}
-                  imgSrc={
-                    typeof image?.profile === 'string'
-                      ? image?.profile
-                      : currentWorkspace?.image_url || ''
-                  }
-                  name={currentWorkspace?.name || ''}
-                />
-              )}
-
+              <Avatar
+                size={58}
+                imgSrc={
+                  typeof image?.profile === 'string'
+                    ? image?.profile
+                    : currentWorkspace?.image_url || ''
+                }
+                name={currentWorkspace?.name || ''}
+              />
               <Frame>
                 <Link onClick={handleUploadClick}>Upload new image</Link>
                 <p>
