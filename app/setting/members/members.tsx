a6c1693f670b465870f6b15b90d8c748515b060e
darshanitaliya7110 <darshan.italiya@pixer.digital>
2024-08-21 11:00:38 +0530

    Update members.tsx

--- app/setting/members/members.tsx
+++ app/setting/members/members.tsx
@@ -2,6 +2,7 @@
 'use client';
 import React, { useCallback, useEffect, useState } from 'react';
 import { observer } from 'mobx-react-lite';
+import { UserRole } from '@prisma/client';
 import {
   Description,
   Head,
@@ -14,18 +15,22 @@ import {
 } from '../style';
 import Button from '@/components/button/button';
 import MemberCard from '@/components/memberCard/memberCard';
-import { getWorkspaceList } from '@/services/clientSide/workspaceServices';
+import {
+  getWorkspaceList,
+  removeMemberFromWorkspace,
+  updateRole,
+} from '@/services/clientSide/workspaceServices';
 import { useStores } from '@/stores';
 import InviteMemberModal from '@/components/inviteMemberModal/inviteMemberModal';
 import Modal from '@/components/modal/modal';
 
 const Members = () => {
   const [inviteModal, setInviteModal] = useState(false);
-  const { workspaceStore } = useStores();
-  const { currentWorkspace } = workspaceStore;
   const [currentOpenDropdown, setCurrentOpenDropdown] = useState<string | null>(
     null,
   );
+  const { workspaceStore } = useStores();
+  const { currentWorkspace } = workspaceStore;
 
   const onOpenInviteModal = useCallback(() => {
     setInviteModal(true);
@@ -34,6 +39,40 @@ const Members = () => {
   const onCloseInviteModal = useCallback(() => {
     setInviteModal(false);
   }, []);
+
+  const handleClick = async (hoveredItem: string | null, userId: string) => {
+    if (hoveredItem === 'Make Admin' || hoveredItem === 'Remove Admin') {
+      try {
+        workspaceStore.setLoading(true);
+        if (userId) {
+          const result = await updateRole({
+            userId,
+            role:
+              hoveredItem === 'Make Admin' ? UserRole.ADMIN : UserRole.MEMBER,
+          });
+
+          if (result) {
+            getWorkspaceMember();
+          }
+        }
+        workspaceStore.setLoading(false);
+      } catch (error) {
+        console.log('error', error);
+      }
+    }
+    if (hoveredItem === 'Delete') {
+      try {
+        if (userId) {
+          const result = await removeMemberFromWorkspace(userId);
+          if (result) {
+            workspaceStore.removeUserFromWorkspace(userId);
+          }
+        }
+      } catch (error) {
+        console.log('error', error);
+      }
+    }
+  };
 
   const getWorkspaceMember = useCallback(async () => {
     workspaceStore.setLoading(true);
@@ -44,7 +83,7 @@ const Members = () => {
 
   useEffect(() => {
     getWorkspaceMember();
-  }, []);
+  }, [inviteModal]);
 
   return (
     <>
@@ -64,14 +103,29 @@ const Members = () => {
               {currentWorkspace?.users?.map((member) => (
                 <MemberCard
                   key={member.id}
+                  userId={member.id}
+                  handleClick={handleClick}
                   name={member.display_name || ''}
                   email={member.email}
                   src={member.profile_url || ''}
-                  designation={''}
+                  designation={member.role}
                   currentOpenDropdown={currentOpenDropdown}
                   setOpenDropdown={setCurrentOpenDropdown}
                   dropdownIdentifier={`card-${member.id}`}
-                  userId={''}
+                />
+              ))}
+              {currentWorkspace?.invited_users?.map((member) => (
+                <MemberCard
+                  key={member.id}
+                  userId={member.id}
+                  handleClick={handleClick}
+                  name={member.name || ''}
+                  email={member.email}
+                  src={''}
+                  designation={'PENDING'}
+                  currentOpenDropdown={currentOpenDropdown}
+                  setOpenDropdown={setCurrentOpenDropdown}
+                  dropdownIdentifier={`card-${member.id}`}
                 />
               ))}
             </MainCardDiv>
