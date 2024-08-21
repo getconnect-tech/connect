819f4e75c955bf49a0b7f6437d8e28bc74c3ed9e
pinal-pixer <pinal.patoliya@pixer.digital>
2024-08-21 10:21:15 +0530

    create same modal component for all modal

--- components/createKeyModal/createKeyModal.tsx
+++ components/createKeyModal/createKeyModal.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import Icon from '../icon/icon';
-import Input from '../input/input';
-import Button from '../button/button';
-import { BottomDiv, Header, Label, MainDiv, Title } from './style';
+import ModalComponent from '../modalComponent/modalComponent';
 
 interface Props {
   onClose: () => void;
@@ -10,27 +7,17 @@ interface Props {
 
 function CreateKeyModal({ onClose }: Props) {
   return (
-    <MainDiv>
-      <Header>
-        <Title>Create API key</Title>
-        <Icon
-          iconName='cross-icon'
-          iconSize='12'
-          iconViewBox='0 0 16 16'
-          onClick={onClose}
-        />
-      </Header>
-      <BottomDiv>
-        <Label>
-          Name <span>(This is just for your reference)</span>
-        </Label>
-        <Input placeholder={'Enter your key name'} className='input' />
-        <div className='button'>
-          <Button type='submit' title='Cancel' secondary={true} />
-          <Button type='submit' title='Create' />
-        </div>
-      </BottomDiv>
-    </MainDiv>
+    <ModalComponent
+      onClose={onClose}
+      onSubmit={() => {}}
+      loading={false}
+      buttonTitle='Create'
+      title={'Create API key'}
+      inputFirstLabel={'Name'}
+      firstPlaceholder='Enter your key name'
+      buttonCancel='Cancel'
+      span='(This is just for your reference)'
+    />
   );
 }
 
