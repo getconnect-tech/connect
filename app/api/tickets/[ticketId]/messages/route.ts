b438903edc25c771d21db00f1f11f1cf43ff0dfa
Vatsal Ghoghari <vatsal.ghoghari@pixer.digital>
2024-08-22 17:47:00 +0530

    implemented support for type EMAIL

--- app/api/tickets/[ticketId]/messages/route.ts
+++ app/api/tickets/[ticketId]/messages/route.ts
@@ -1,8 +1,10 @@
 import { z } from 'zod';
+import { MessageType } from '@prisma/client';
 import { handleApiError } from '@/helpers/errorHandler';
 import withWorkspaceAuth from '@/middlewares/withWorkspaceAuth';
 import { getTicketMessages, postMessage } from '@/services/serverSide/message';
 import { contentSchema, messageTypeSchema } from '@/lib/zod/message';
+import { sendEmailAsReply } from '@/helpers/emails';
 
 export const GET = withWorkspaceAuth(async (req, { ticketId }) => {
   try {
@@ -24,17 +26,39 @@ export const POST = withWorkspaceAuth(async (req, { ticketId }) => {
 
     RequestBody.parse(requestBody);
 
-    const { content, type } = requestBody;
+    const { content, type } = requestBody as z.infer<typeof RequestBody>;
 
-    const newMessage = await postMessage({
-      messageContent: content,
-      messageType: type,
-      referenceId: '',
-      ticketId,
-      authorId: req.user.id,
-    });
+    if (type === MessageType.REGULAR) {
+      const newMessage = await postMessage({
+        messageContent: content,
+        messageType: type,
+        referenceId: '',
+        ticketId,
+        authorId: req.user.id,
+      });
 
-    return Response.json(newMessage, { status: 201 });
+      return Response.json(newMessage, { status: 201 });
+    }
+
+    if (type === MessageType.EMAIL) {
+      const mailId = await sendEmailAsReply(ticketId, content);
+
+      if (!mailId) {
+        return Response.json({ error: 'Ticket not found!' }, { status: 404 });
+      }
+
+      const newMessage = await postMessage({
+        messageContent: content,
+        messageType: type,
+        referenceId: mailId,
+        ticketId: ticketId,
+        authorId: req.user.id,
+      });
+
+      return Response.json(newMessage, { status: 201 });
+    }
+
+    return Response.json({ error: 'Invalid message type!' }, { status: 400 });
   } catch (err) {
     return handleApiError(err);
   }
