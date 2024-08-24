5ebcb12cd44fb1529603dd60305ef1f5f925d1b1
darshanitaliya7110 <darshan.italiya@pixer.digital>
2024-08-24 15:40:50 +0530

    create message obj and set in mobX

--- app/details/[ticket_id]/ticketDetails.tsx
+++ app/details/[ticket_id]/ticketDetails.tsx
@@ -39,7 +39,7 @@ import {
   updateTicketPriority,
   sendMessage,
 } from '@/services/clientSide/ticketServices';
-import { capitalizeString, isEmpty } from '@/helpers/common';
+import { capitalizeString, getUniqueId, isEmpty } from '@/helpers/common';
 import Icon from '@/components/icon/icon';
 import RichTextBox from '@/components/commentBox';
 import { DropDownItem } from '@/components/dropDown/dropDown';
@@ -248,7 +248,6 @@ function TicketDetails(props: Props) {
     },
     [ticketDetails],
   );
-
   /*
    * @desc Send comment
    */
@@ -261,11 +260,24 @@ function TicketDetails(props: Props) {
         type = MessageType.EMAIL;
       }
       const payload = { content: content, type };
+      const newMessage: MessageDetails = {
+        assignee: null,
+        author: user,
+        author_id: user!.id,
+        content,
+        id: getUniqueId(),
+        created_at: new Date(),
+        label: null,
+        reference_id: '',
+        ticket_id,
+        type,
+      };
+
       try {
         if (ticket_id) {
+          ticketStore.setTicketMessages([...messages, newMessage]);
           const result = await sendMessage(ticket_id, payload);
           if (result) {
-            getTicketMessages(ticket_id);
             setCommentValue('');
           }
         }
