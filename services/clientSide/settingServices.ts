1951178e268f0613910edf27911085eb7444690e
jemin-pixer <jemin.rudani@pixer.digital>
2024-08-27 14:14:52 +0530

    create service get all API key

--- services/clientSide/settingServices.ts
+++ services/clientSide/settingServices.ts
@@ -1,8 +1,32 @@
 /* eslint-disable no-undef */
 import axios from 'axios';
-import { workspaceStore } from '@/stores/workspaceStore';
 import { NEXT_PUBLIC_API_URL } from '@/helpers/environment';
 import { getAPIErrorMessage } from '@/helpers/common';
+import { workspaceStore } from '@/stores/workspaceStore';
+
+/**
+ * @desc Get all API key
+ * @param {*}
+ */
+export const getAPIKeys = async () => {
+  try {
+    workspaceStore.setLoading(true);
+    const response = await axios.get(
+      `${NEXT_PUBLIC_API_URL}/workspaces/apikeys`,
+    );
+    const { data } = response;
+    if (data?.length > 0) {
+      workspaceStore.setAPIKeys(data);
+      return data;
+    }
+    return data;
+  } catch (err: any) {
+    alert(getAPIErrorMessage(err) || 'Something went wrong!');
+    return null;
+  } finally {
+    workspaceStore.setLoading(false);
+  }
+};
 
 /**
  * @desc Add label
