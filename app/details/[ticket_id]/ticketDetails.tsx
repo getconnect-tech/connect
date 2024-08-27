0d59b297089a23148488cf15fd9f588f54a54938
darshanitaliya7110 <darshan.italiya@pixer.digital>
2024-08-27 11:52:41 +0530

    add condition in onChangePriority

--- app/details/[ticket_id]/ticketDetails.tsx
+++ app/details/[ticket_id]/ticketDetails.tsx
@@ -187,20 +187,25 @@ function TicketDetails(props: Props) {
   const onChangePriority = useCallback(
     async (item: { name: string; icon: string; value: PriorityLevels }) => {
       const payload = { priority: item?.value };
-      try {
-        if (ticketDetails?.id) {
-          const updatedTicketDetails = {
-            ...ticketDetails,
-            priority: item?.value,
-          };
-          ticketStore.setTicketDetails(updatedTicketDetails);
-          const result = await updateTicketPriority(ticketDetails?.id, payload);
-          if (result) {
-            loadData();
+      if (ticketDetails?.priority !== item?.value) {
+        try {
+          if (ticketDetails?.id) {
+            const updatedTicketDetails = {
+              ...ticketDetails,
+              priority: item?.value,
+            };
+            ticketStore.setTicketDetails(updatedTicketDetails);
+            const result = await updateTicketPriority(
+              ticketDetails?.id,
+              payload,
+            );
+            if (result) {
+              loadData();
+            }
           }
+        } catch (e) {
+          console.log('Error : ', e);
         }
-      } catch (e) {
-        console.log('Error : ', e);
       }
     },
     [ticketDetails],
