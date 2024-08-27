a387466c3be4c80ae50fa3a1c337dfe867f64200
Vatsal Ghoghari <vatsal.ghoghari@pixer.digital>
2024-08-27 18:24:12 +0530

    implemented service and route to delete api key

--- services/serverSide/apiKey.ts
+++ services/serverSide/apiKey.ts
@@ -35,3 +35,11 @@ export const getApiDetails = async (apiKey: string) => {
 
   return apiDetails;
 };
+
+export const deleteApiKey = async (apiKey: string) => {
+  const deletedApiKey = await prisma.apiKeys.delete({
+    where: { api_key: apiKey },
+  });
+
+  return deletedApiKey;
+};
