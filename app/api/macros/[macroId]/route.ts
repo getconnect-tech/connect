a299e95226f26d6457c08ae0fa113f5708367ce8
pixer-varship <varship.pokal@pixer.digital>
2024-08-30 18:30:02 +0530

    origin/feature/create-service-for-put-macro

--- app/api/macros/[macroId]/route.ts
+++ app/api/macros/[macroId]/route.ts
@@ -2,16 +2,16 @@ import { z } from 'zod';
 import { handleApiError } from '@/helpers/errorHandler';
 import { createRequestBody } from '@/lib/zod/macro';
 import withAdminAuth from '@/middlewares/withAdminAuth';
-import { createOrUpdateMacro as createMacro } from '@/services/serverSide/macro';
+import { createOrUpdateMacro as updateMacro } from '@/services/serverSide/macro';
 
-export const POST = withAdminAuth(async (req) => {
+export const PUT = withAdminAuth(async (req, { macroId }) => {
   try {
     const requestBody = await req.json();
     createRequestBody.parse(requestBody);
     const { title, content } = requestBody as z.infer<typeof createRequestBody>;
     const userId = req.user.id;
     const workspaceId = req.workspace.id;
-    const macro = await createMacro(userId, workspaceId, '', {
+    const macro = await updateMacro(userId, workspaceId, macroId, {
       title,
       content,
     });
--- services/serverSide/macro.ts
+++ services/serverSide/macro.ts
@@ -1,9 +1,10 @@
 import { Macro } from '@prisma/client';
 import { prisma } from '@/prisma/prisma';
 
-export const createMacro = async (
+export const createOrUpdateMacro = async (
   user_id: string,
   workspace_id: string,
+  macroId: string,
   model: {
     title: string;
     content: string;
@@ -15,12 +16,15 @@ export const createMacro = async (
       workspace_id,
       created_by: user_id,
     };
-    const macro = await prisma.macro.create({
-      data: payload,
+
+    const macro = await prisma.macro.upsert({
+      where: { id: macroId },
+      update: payload,
+      create: payload,
     });
     return macro;
   } catch (e) {
-    console.log('Error create Macro:', e);
+    console.log('Error creating or updating Macro:', e);
     throw e;
   }
 };
