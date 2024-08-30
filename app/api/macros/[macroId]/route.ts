ba545ebdcfd6214e4712aee58225af5533880f0f
pixer-varship <varship.pokal@pixer.digital>
2024-08-30 18:50:32 +0530

    origin/feature/create-service-for-delete-macro

--- app/api/macros/[macroId]/route.ts
+++ app/api/macros/[macroId]/route.ts
@@ -2,7 +2,10 @@ import { z } from 'zod';
 import { handleApiError } from '@/helpers/errorHandler';
 import { createRequestBody } from '@/lib/zod/macro';
 import withAdminAuth from '@/middlewares/withAdminAuth';
-import { createOrUpdateMacro as updateMacro } from '@/services/serverSide/macro';
+import {
+  createOrUpdateMacro as updateMacro,
+  deleteMacro,
+} from '@/services/serverSide/macro';
 
 export const PUT = withAdminAuth(async (req, { macroId }) => {
   try {
@@ -16,6 +19,17 @@ export const PUT = withAdminAuth(async (req, { macroId }) => {
       content,
     });
     return Response.json(macro, { status: 201 });
+  } catch (err) {
+    return handleApiError(err);
+  }
+});
+
+export const DELETE = withAdminAuth(async (req, { macroId }) => {
+  try {
+    const result = await deleteMacro(macroId);
+    if (result)
+      return Response.json('Macro successfully deleted.', { status: 201 });
+    else throw new Error('Failed to delete macro.');
   } catch (err) {
     return handleApiError(err);
   }
--- services/serverSide/macro.ts
+++ services/serverSide/macro.ts
@@ -1,5 +1,6 @@
 import { Macro } from '@prisma/client';
 import { prisma } from '@/prisma/prisma';
+import { isEmpty } from '@/helpers/common';
 
 export const createOrUpdateMacro = async (
   user_id: string,
@@ -25,6 +26,20 @@ export const createOrUpdateMacro = async (
     return macro;
   } catch (e) {
     console.log('Error creating or updating Macro:', e);
+    throw e;
+  }
+};
+
+export const deleteMacro = async (macroId: string): Promise<boolean> => {
+  try {
+    if (isEmpty(macroId)) throw new Error("'macroId' is required!");
+    const result = await prisma.macro.delete({
+      where: { id: macroId },
+    });
+    if (result) return true;
+    return false;
+  } catch (e) {
+    console.log('Error deleting Macro:', e);
     throw e;
   }
 };
