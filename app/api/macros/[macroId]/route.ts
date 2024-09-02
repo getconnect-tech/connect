bc9040c392d648d30234a37db7845a0fc31be74d
pixer-varship <varship.pokal@pixer.digital>
2024-09-02 14:32:32 +0530

    Update route.ts

--- app/api/macros/[macroId]/route.ts
+++ app/api/macros/[macroId]/route.ts
@@ -1,14 +1,11 @@
-import { z } from 'zod';
 import { handleApiError } from '@/helpers/errorHandler';
-import { createRequestBody } from '@/lib/zod/macro';
 import withAdminAuth from '@/middlewares/withAdminAuth';
 import { createOrUpdateMacro as updateMacro } from '@/services/serverSide/macro';
 
 export const PUT = withAdminAuth(async (req, { macroId }) => {
   try {
     const requestBody = await req.json();
-    createRequestBody.parse(requestBody);
-    const { title, content } = requestBody as z.infer<typeof createRequestBody>;
+    const { title, content } = requestBody;
     const userId = req.user.id;
     const workspaceId = req.workspace.id;
     const macro = await updateMacro(userId, workspaceId, macroId, {
