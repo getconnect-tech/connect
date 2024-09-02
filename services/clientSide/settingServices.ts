85bf31801697e26cde25de7c7ff7ff71dcaf19d2
jemin-pixer <jemin.rudani@pixer.digital>
2024-09-02 14:10:37 +0530

    Update settingServices.ts

--- services/clientSide/settingServices.ts
+++ services/clientSide/settingServices.ts
@@ -149,6 +149,26 @@ export const deleteLabel = async (labelId: string) => {
 };
 
 /**
+ * @desc  create macros
+ * @param {*} payload { "title" , "content"}
+ */
+export const createMacros = async (payload: object) => {
+  try {
+    settingStore.setLoading(true);
+    const response = await axios.post(`${NEXT_PUBLIC_API_URL}/macros`, payload);
+    const { data } = response;
+    return data;
+  } catch (err: any) {
+    messageStore.setErrorMessage(
+      getAPIErrorMessage(err) || 'Something went wrong!',
+    );
+    return null;
+  } finally {
+    settingStore.setLoading(false);
+  }
+};
+
+/**
  * @desc  update macros
  * @param {*} payload { "title" , "content"}
  */
