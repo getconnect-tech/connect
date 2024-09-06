ed0a5ef5f1fc25b5c78f3b622c62ac80543fac95
darshanitaliya7110 <darshan.italiya@pixer.digital>
2024-09-06 10:50:06 +0530

    create handleChangeSnooze function and use it

--- components/contextMenu/contextMenu.tsx
+++ components/contextMenu/contextMenu.tsx
@@ -164,12 +164,43 @@ export default function CustomContextMenu(props: Props) {
     [ticketDetail],
   );
 
+  /*
+   * @desc update ticket snooze
+   */
+  const handleChangeSnooze = useCallback(
+    async (props: HandleClickProps) => {
+      const { item } = props;
+      const payload = { status: TicketStatus.OPEN, snoozeUntil: item?.value };
+      try {
+        if (ticketDetail?.id) {
+          const updatedTicketDetails = {
+            ...(ticketDetail || {}),
+            status: TicketStatus.OPEN,
+            snooze_until: new Date(item?.value || ''),
+          };
+          // add data in mobX store
+          ticketStore.setTicketDetails(updatedTicketDetails);
+          // api call for change ticket status
+          await changeTicketStatus(ticketDetail?.id, payload);
+        }
+      } catch (e) {
+        console.log('Error : ', e);
+      }
+    },
+    [ticketDetail],
+  );
+
   const [showDropDown, setShowDropDown] = useState(true);
   const [showDatePicker, setShowDatePicker] = useState(false);
 
-  const handleDropDownChange = () => {
-    setShowDropDown(false);
-    setShowDatePicker(true);
+  const handleDropDownChange = (item: any) => {
+    if (item?.name === 'date&time') {
+      setShowDropDown(false);
+      setShowDatePicker(true);
+    } else {
+      setShowDropDown(true);
+      setShowDatePicker(false);
+    }
   };
 
   const handleDatePickerClose = () => {
@@ -261,15 +292,17 @@ export default function CustomContextMenu(props: Props) {
                           items={snoozeItem}
                           iconSize='12'
                           iconViewBox='0 0 12 12'
-                          onClose={() => setShowDropDown(false)}
+                          onClose={() => {}}
                           isContextMenu={true}
                           isSnooze={true}
                           style={{ minWidth: 260, marginTop: -4 }}
                           onChange={handleDropDownChange}
+                          handleClick={handleChangeSnooze}
                         />
                       )}
                       {showDatePicker && (
                         <DatePickerModal
+                          ticketDetails={ticketDetail}
                           onClose={handleDatePickerClose}
                           isContextMenu={true}
                         />
