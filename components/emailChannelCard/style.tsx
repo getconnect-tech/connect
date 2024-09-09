a8f6b0124f71e58d5bd31e6d488b565ab27da1ac
laxitarajpurohit <laxita.rajpurohit@pixer.digital>
2024-09-09 14:28:14 +0530

    fix eslint error

--- components/emailChannelCard/style.tsx
+++ components/emailChannelCard/style.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 /* eslint-disable prettier/prettier */
 /* eslint-disable max-len */
 import styled, { css } from 'styled-components';
@@ -119,9 +120,7 @@ export const StyledCheckbox = styled.input.attrs({ type: 'checkbox' })`
   &:checked {
     background-color: var(--brand);
     // eslint-disable-next-line prettier/prettier
-    background-image: url('data:image/svg+xml;utf8,${encodeURIComponent(
-      checkmarkSVG,
-    )}');
+    background-image: url('data:image/svg+xml;utf8,${encodeURIComponent(checkmarkSVG)}');
     background-size: 10px 10px;
     background-repeat: no-repeat;
     background-position: center;
