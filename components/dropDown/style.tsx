37a82661eb535f69d432322b0abc32d9d1f4fe4d
laxitarajpurohit <laxita.rajpurohit@pixer.digital>
2024-09-14 18:01:21 +0530

    Update style.tsx

--- components/dropDown/style.tsx
+++ components/dropDown/style.tsx
@@ -113,9 +113,7 @@ const StyledCheckbox = styled.input.attrs({ type: 'checkbox' })`
 
   &:checked {
     background-color: var(--brand);
-    background-image: url('data:image/svg+xml;utf8,${encodeURIComponent(
-      checkmarkSVG,
-    )}');
+    background-image: url('data:image/svg+xml;utf8,${encodeURIComponent(checkmarkSVG)}');
     background-size: 10px 10px;
     background-repeat: no-repeat;
     background-position: center;
