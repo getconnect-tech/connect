a02d51f0f4f97f2fb990cbfa9e48954c9c02b39a
darshanitaliya7110 <darshan.italiya@pixer.digital>
2024-09-18 16:24:40 +0530

    update export type

--- lib/openAi.ts
+++ lib/openAi.ts
@@ -5,8 +5,6 @@ const openAiAPIStr = process.env.OPENAI_API_KEY;
 if (!openAiAPIStr) {
   throw new Error('Please define `OPENAI_API_KEY` in .env');
 }
-const openAi = new OpenAI({
+export const openAi = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
-export default openAi;
