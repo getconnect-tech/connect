5b68551f5c28d319b5cfef908937353725250239
darshanitaliya7110 <darshan.italiya@pixer.digital>
2024-09-18 16:20:07 +0530

    Update openAi.ts

--- lib/openAi.ts
+++ lib/openAi.ts
@@ -1,5 +1,10 @@
 import OpenAI from 'openai';
 
+const openAiAPIStr = process.env.OPENAI_API_KEY;
+
+if (!openAiAPIStr) {
+  throw new Error('Please define `OPENAI_API_KEY` in .env');
+}
 const openAi = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
