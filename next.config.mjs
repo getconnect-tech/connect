0bff4d74af1c5304bf255423f90dfa06ab75d4c2
Vatsal Ghoghari <vatsal.ghoghari@pixer.digital>
2024-09-24 17:39:10 +0530

    update nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -8,23 +8,19 @@ const nextConfig = {
     return [
       {
         // Routes this applies to
-        source: '/api/(.*)',
+        source: '/api/:path',
         // Headers
         headers: [
-          // Allow for specific domains to have access or * for all
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          // Allows for specific methods accepted
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
           {
             key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
           },
-          // Allows for specific headers accepted (These are a few standard ones)
           {
             key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type, Authorization',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
           },
         ],
       },
