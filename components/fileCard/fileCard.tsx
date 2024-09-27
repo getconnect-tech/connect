393f938c5459102b0efe26289c9b505f1853745c
pinal-pixer <pinal.patoliya@pixer.digital>
2024-09-27 17:00:49 +0530

    add files icon

--- components/fileCard/fileCard.tsx
+++ components/fileCard/fileCard.tsx
@@ -17,22 +17,88 @@ interface Props {
   fileName: string;
   url?: string;
 }
+
 export default function FileCard({
   documentText,
   fileSize,
   fileName,
   url,
 }: Props) {
+  // Function to get the appropriate icon based on the file extension
+  const getFileIcon = () => {
+    const fileExtension = fileName.split('.').pop()?.toLowerCase();
+
+    switch (fileExtension) {
+      case 'pdf':
+        return (
+          <SVGIcon
+            name='pdf-icon'
+            width='20'
+            height='20'
+            viewBox='0 0 16 16'
+            className='file-icon'
+          />
+        );
+      case 'doc':
+      case 'docx':
+        return (
+          <SVGIcon
+            name='doc-icon'
+            width='20'
+            height='20'
+            viewBox='0 0 16 16'
+            className='file-icon'
+          />
+        );
+      case 'csv':
+        return (
+          <SVGIcon
+            name='csv-icon'
+            width='20'
+            height='20'
+            viewBox='0 0 16 16'
+            className='file-icon'
+          />
+        );
+      case 'xls':
+      case 'xlsx':
+        return (
+          <SVGIcon
+            name='excel-icon'
+            width='20'
+            height='20'
+            viewBox='0 0 16 16'
+            className='file-icon'
+          />
+        );
+      case 'mp4':
+        return (
+          <SVGIcon
+            name='mp4-icon'
+            width='20'
+            height='20'
+            viewBox='0 0 16 16'
+            className='file-icon'
+          />
+        );
+      default:
+        return (
+          <SVGIcon
+            name='common-file-icon'
+            width='20'
+            height='20'
+            viewBox='0 0 16 16'
+            className='file-icon'
+          />
+        );
+    }
+  };
+
   return (
     <FileCardDiv>
       <Content>
-        <SVGIcon
-          name='pdf-icon'
-          width='20px'
-          height='20px'
-          viewBox='0 0 20 20'
-          className='file-icon'
-        />
+        {/* Show appropriate icon based on file type */}
+        {getFileIcon()}
         <FileDetail>
           <FileName>{fileName}</FileName>
           <FileSize className='file-size'>
