adc4ae4aa2db86f796c5e5a0ee3fdc92e711d744
laxitarajpurohit <laxita.rajpurohit@pixer.digital>
2024-09-28 12:32:08 +0530

    fixed the ol and ul style

--- components/prosemirror/style.tsx
+++ components/prosemirror/style.tsx
@@ -3,6 +3,14 @@ import styled from 'styled-components';
 const EditorDiv = styled.div`
   max-height: 240px;
   min-height: 40px;
+  width: 100%;
+  font-size: 14px !important;
+  color: var(--text) !important;
+  font-family: unset !important;
+  white-space: normal;
+  p {
+    word-break: break-word;
+  }
   &:focus-visible {
     outline: none !important;
   }
@@ -15,6 +23,10 @@ const EditorDiv = styled.div`
     }
   }
   .ProseMirror ul {
+    display: inline-block;
+    margin-left: 20px;
+  }
+  .ProseMirror ol {
     display: inline-block;
     margin-left: 20px;
   }
