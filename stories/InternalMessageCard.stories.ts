c04a2ce7566e5ff79397aa7ba10328fe38a94b26
pinal-pixer <pinal.patoliya@pixer.digital>
2024-09-28 15:43:30 +0530

    create intetnal message card component in storybook

---
+++ stories/InternalMessageCard.stories.ts
@@ -0,0 +1,52 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import InternalMessageCard from '../components/internalMessageCard/internalMessageCard';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Example/InternalMessageCard',
+  component: InternalMessageCard,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof InternalMessageCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    title: '@Laxita Hello, Good morning',
+    time: new Date('2023-09-24T12:34:56Z'),
+    reactions: [{}],
+    messageId: '1',
+  },
+};
+
+export const PrimaryWithReaction: Story = {
+  args: {
+    title: '@Laxita Hello, Good morning',
+    time: new Date('2023-09-24T12:34:56Z'),
+    reactions: [
+      {
+        emoji: '👍',
+        count: 10,
+        author: [
+          { id: '1', display_name: 'Pinal' },
+          { id: '2', display_name: 'Laxita' },
+        ],
+      },
+      {
+        emoji: '❤️',
+        count: 5,
+        author: [
+          { id: '1', display_name: 'Pinal' },
+          { id: '2', display_name: 'Laxita' },
+        ],
+      },
+    ],
+    messageId: '2',
+    showReactions: true,
+  },
+};
