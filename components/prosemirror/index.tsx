116a0064abd1856c6dccad13d0cdb068efc5c71f
aniket-pixer <aniket@pixer.digital>
2024-09-30 18:54:56 +0530

    Prosemirror editor - add logic for embed image

--- components/prosemirror/index.tsx
+++ components/prosemirror/index.tsx
@@ -1,5 +1,11 @@
-import { forwardRef, useEffect, useImperativeHandle, useRef } from 'react';
-import { EditorState } from 'prosemirror-state';
+import {
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from 'react';
+import { EditorState, PluginKey } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 import {
   Schema,
@@ -9,6 +15,9 @@ import {
 import { schema } from 'prosemirror-schema-basic';
 import { addListNodes } from 'prosemirror-schema-list';
 import { exampleSetup } from 'prosemirror-example-setup';
+import { Plugin } from 'prosemirror-state';
+import { Decoration, DecorationSet } from 'prosemirror-view';
+import { getFirebaseUrlFromFile } from '@/helpers/common';
 
 interface Props {
   // eslint-disable-next-line no-unused-vars
@@ -32,6 +41,98 @@ const ProsemirrorEditor = forwardRef((props: Props, ref) => {
     return div.innerHTML;
   };
 
+  const placeholderPluginKey = new PluginKey('placeholderPlugin');
+
+  // eslint-disable-next-line prefer-const
+  let placeholderPlugin = new Plugin({
+    key: placeholderPluginKey, // Assign the key here
+    state: {
+      init() {
+        return DecorationSet.empty;
+      },
+      apply(tr, set) {
+        set = set.map(tr.mapping, tr.doc);
+
+        // Use the plugin key to get meta information
+        const action = tr.getMeta(placeholderPluginKey);
+
+        if (action && action.add) {
+          // eslint-disable-next-line no-undef
+          const widget = document.createElement('placeholder');
+          const deco = Decoration.widget(action.add.pos, widget, {
+            id: action.add.id,
+          });
+          set = set.add(tr.doc, [deco]);
+        } else if (action && action.remove) {
+          set = set.remove(
+            set.find(
+              undefined,
+              undefined,
+              (spec) => spec.id === action.remove.id,
+            ),
+          );
+        }
+        return set;
+      },
+    },
+    props: {
+      decorations(state) {
+        return this.getState(state);
+      },
+    },
+  });
+
+  // const findPlaceholder = useCallback(
+  //   (state: any, id: any) => {
+  //     const decos = placeholderPlugin.getState(state);
+  //     const found = decos?.find(null, null, (spec) => spec.id === id);
+  //     return found?.length ? found[0].from : null;
+  //   },
+  //   [placeholderPlugin],
+  // );
+
+  // async function uploadFile(file: any) {
+  //   // Simulate an upload and return a URL. Replace this with actual file upload logic.
+  //   return new Promise((resolve) => {
+  //     setTimeout(() => {
+  //       resolve(URL.createObjectURL(file)); // Returns a local URL for the image (for demo purposes)
+  //     }, 1500); // Simulating an upload delay
+  //   });
+  // }
+
+  const startImageUpload = useCallback(
+    (view: any, file: any, uploadPath: string, fileName: string) => {
+      const id = {};
+      // Replace the selection with a placeholder
+      const tr = view.state.tr;
+      if (!tr.selection.empty) tr.deleteSelection();
+      tr.setMeta(placeholderPlugin, { add: { id, pos: tr.selection.from } });
+      view.dispatch(tr);
+
+      getFirebaseUrlFromFile(file, uploadPath, fileName)
+        .then((url) => {
+          // const pos = findPlaceholder(view.state, id);
+          const pos = tr.selection.from;
+          if (pos === null) return;
+
+          // Replace the placeholder with the uploaded image
+          view.dispatch(
+            view.state.tr
+              .replaceWith(
+                pos,
+                pos,
+                view.state.schema.nodes.image.create({ src: url }),
+              )
+              .setMeta(placeholderPlugin, { remove: { id } }),
+          );
+        })
+        .catch(() => {
+          view.dispatch(tr.setMeta(placeholderPlugin, { remove: { id } }));
+        });
+    },
+    [placeholderPlugin, viewRef.current],
+  );
+
   useEffect(() => {
     if (!editorRef.current || !contentRef.current) return;
 
@@ -47,7 +148,7 @@ const ProsemirrorEditor = forwardRef((props: Props, ref) => {
         doc: ProseMirrorDOMParser.fromSchema(mySchema).parse(
           contentRef.current,
         ),
-        plugins: exampleSetup({ schema: mySchema }),
+        plugins: [...exampleSetup({ schema: mySchema }), placeholderPlugin],
       }),
       // Capture the update event to handle editor state changes
       dispatchTransaction(transaction) {
@@ -78,8 +179,13 @@ const ProsemirrorEditor = forwardRef((props: Props, ref) => {
           plugins: viewRef.current.state.plugins,
         });
         viewRef.current.updateState(emptyState);
-        // setEditorHTML(''); // Clear the HTML state as well
         setValueContent('');
+      }
+    },
+    uploadFile(file: any, uploadPath: string, fileName: string) {
+      if (file && viewRef.current?.state.selection.$from.parent.inlineContent) {
+        viewRef.current.focus();
+        startImageUpload(viewRef.current, file, uploadPath, fileName);
       }
     },
   }));
