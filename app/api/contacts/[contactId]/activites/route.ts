d1f93a17342bede527872f1c0b44cbf792c5b931
Vatsal Ghoghari <vatsal.ghoghari@pixer.digital>
2024-10-03 11:58:22 +0530

    implemented API route and service to get user recent activites

---
+++ app/api/contacts/[contactId]/activites/route.ts
@@ -0,0 +1,19 @@
+import { handleApiError } from '@/helpers/errorHandler';
+import withWorkspaceAuth from '@/middlewares/withWorkspaceAuth';
+import { getActivities, getContactById } from '@/services/serverSide/contact';
+
+export const GET = withWorkspaceAuth(async (req, { contactId }) => {
+  try {
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      return Response.json({ error: 'Contact not found!' }, { status: 404 });
+    }
+
+    const events = await getActivities(contact.email);
+
+    return Response.json(events || [], { status: 200 });
+  } catch (err) {
+    return handleApiError(err);
+  }
+});
