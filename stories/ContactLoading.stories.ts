083af315e31b00bd4843ab571b0bd58ff5f15534
pinal-pixer <pinal.patoliya@pixer.digital>
2024-10-08 14:48:15 +0530

    Update ContactLoading.stories.ts

--- stories/ContactLoading.stories.ts
+++ stories/ContactLoading.stories.ts
@@ -14,7 +14,6 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {},
 };
